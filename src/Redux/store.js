1fb9f8567a23457787cf737c8357565fad464395
Vadim luginets <pebeeo@gmail.com>
2022-12-07 15:07:30 +0200

    Added persist storage for user slice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,16 +1,41 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
 import {
   userReducer,
   logInReduser,
   registerReducer,
   addContactFormRducer,
 } from './slices';
-
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+const persistConfig = {
+  key: 'user',
+  storage,
+};
+const persistedContactsSliceReducer = persistReducer(
+  persistConfig,
+  userReducer
+);
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedContactsSliceReducer,
     register: registerReducer,
     logIn: logInReduser,
     addContactForm: addContactFormRducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from 'Redux/store';
+import { persistor, store } from 'Redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter basename="goit-react-hw-08-phonebook">
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
